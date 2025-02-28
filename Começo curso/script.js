f24cc2337845027d4356b996d80c6fa77ce3bf52
Sullivan Bueno <sullivanbhs@gmail.com>
2025-02-28 05:46:36 -0300

    arquivo atualizado

---
+++ Começo curso/script.js
@@ -0,0 +1,7 @@
+//  var, const e let;
+// var é redeclarada e reatribuida;
+// const nao é redeclarada e nem reatribuida;
+// let nao é redeclarada mas é reatribuida;
+// Exponenciação é feito com "**";
+let exponenciacao = 2 ** 3; 
+console.log(exponenciacao);
