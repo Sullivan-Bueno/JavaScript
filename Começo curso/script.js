90f74d03e865653b46489e9b0180d6722b905b0c
Sullivan Bueno <sullivanbhs@gmail.com>
2025-03-02 22:03:11 -0300

    update

--- Começo curso/script.js
+++ Começo curso/script.js
@@ -3,5 +3,29 @@
 // const nao é redeclarada e nem reatribuida;
 // let nao é redeclarada mas é reatribuida;
 // Exponenciação é feito com "**";
-let exponenciacao = 2 ** 3; 
-console.log(exponenciacao);
+// let exponenciacao = 2 ** 3;
+// console.log(exponenciacao);
+
+// for (let i = 1; i < 11; i++) {
+//   console.log(i);
+// }
+
+// Operadores lógicos && (e), || (ou) e !(not);
+
+// Array
+let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Matriz
+let matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// Objeto
+let pessoa = {
+  nome: "Sullivan",
+  idade: 19,
+  sexo: "masculino",
+};
+console.log(pessoa.idade);
