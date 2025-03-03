fda2d448f8b722f6261ee9a6d1e008193c91864d
Sullivan Bueno <sullivanbhs@gmail.com>
2025-03-02 22:22:14 -0300

    Update exercicio1

---
+++ Começo curso/exercicio1.js
@@ -0,0 +1,49 @@
+// let produtos = ["coca cola", "guaraná", "fanta"];
+// console.log(produtos[1]);
+// produtos[2] = "notebook";
+// console.log(produtos);
+
+// let clientes = ["Sullivan", "Maria", "João"];
+// console.log(clientes);
+// clientes[3] = "Pedro";
+// console.log(clientes);
+// delete clientes[3];
+// console.log(clientes);
+
+// let produto = {
+//   nome: "Notebook",
+//   preco: 1000,
+//   quantidade: 5,
+// }
+
+// console.log(produto.nome);
+// produto.preco = 2000;
+// console.log(produto);
+
+// let venda = {
+//   cliente: "Sullivan",
+//   produto: "Notebook",
+//   valor: 1000,
+// }
+
+// // venda.data = "01/01/2021";
+// // delete venda.produto;
+// // console.log(venda);
+
+// // let estoque = ["notebook", "mouse", "teclado"];
+// // let estoque2 = [...estoque];
+
+// // estoque2[2] = "webcam";
+// // console.log(estoque);
+// // console.log(estoque2);
+
+// // let funcionario = {
+// //   nome: "Sullivan",
+// //   idade: 19,
+// //   sexo: "masculino",
+// // }
+
+// // let funcionario2 = {...funcionario};
+// // funcionario2.idade = 20;
+// // console.log(funcionario);
+// // console.log(funcionario2);
--- Começo curso/script.js
+++ Começo curso/script.js
@@ -1,31 +1,39 @@
-//  var, const e let;
-// var é redeclarada e reatribuida;
-// const nao é redeclarada e nem reatribuida;
-// let nao é redeclarada mas é reatribuida;
-// Exponenciação é feito com "**";
-// let exponenciacao = 2 ** 3;
-// console.log(exponenciacao);
+// //  var, const e let;
+// // var é redeclarada e reatribuida;
+// // const nao é redeclarada e nem reatribuida;
+// // let nao é redeclarada mas é reatribuida;
+// // Exponenciação é feito com "**";
+// // let exponenciacao = 2 ** 3;
+// // console.log(exponenciacao);
 
-// for (let i = 1; i < 11; i++) {
-//   console.log(i);
-// }
+// // for (let i = 1; i < 11; i++) {
+// //   console.log(i);
+// // }
 
-// Operadores lógicos && (e), || (ou) e !(not);
+// // Operadores lógicos && (e), || (ou) e !(not);
 
-// Array
-let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // Array
+// let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Matriz
-let matriz = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+// // Matriz
+// let matriz = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
 
-// Objeto
-let pessoa = {
-  nome: "Sullivan",
-  idade: 19,
-  sexo: "masculino",
-};
-console.log(pessoa.idade);
+// // Objeto
+// let pessoa = {
+//   nome: "Sullivan",
+//   idade: 19,
+//   sexo: "masculino",
+// };
+// // console.log(pessoa.idade);
+
+// // Shadow copy
+// let lista2 = [...lista];
+// console.log(lista2);
+
+// lista2[10] = 11;
+// console.log(lista);
+// console.log(lista2);
