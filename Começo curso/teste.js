c4b906d1db96301c30b6110f66ef6e2affb6fb6a
Sullivan Bueno <sullivanbhs@gmail.com>
2025-03-05 06:09:08 -0300

    atualização teste

--- Começo curso/teste.js
+++ Começo curso/teste.js
@@ -1,16 +1,18 @@
-let inicio = 13;
-let fim = 130;
+let inicio = 18;
+let fim = 200;
 
 function pares(inicio, fim) {
   let j = 0;
   let lista = [];
-  for (let i = inicio; i < fim; i++) {
+  for (let i = inicio; i <= fim; i++) {
     if (i % 2 == 0) {
       lista[j] = [i];
       j++;
     }
   }
-  console.log(lista);
+  return lista;
 }
 
-pares(inicio, fim);
+let resultado = pares(inicio, fim);
+
+console.log(resultado);
