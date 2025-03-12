6ec1fd407eea981e7bd0f8cbbc36b0668384df1f
Sullivan Bueno <sullivanbhs@gmail.com>
2025-03-12 06:39:54 -0300

    Metodos, funcoes e strings

---
+++ Começo curso/metodos.js
@@ -0,0 +1,12 @@
+//  String - sequencia de caracteres
+
+let string = "Olá, mundo!";
+
+
+console.log(string.toLowerCase());
+
+// slice(start, end);
+
+console.log(string.slice(0, 3)); // numeros negativos vão de tras pra frente
+
+console.log(string.substring(0, 3)); // não aceita indice negativo
