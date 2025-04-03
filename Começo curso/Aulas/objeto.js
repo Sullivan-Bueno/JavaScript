fc771a820fec39c5d2610f27846d2acce937ec1b
Sullivan Bueno <sullivanbhs@gmail.com>
2025-04-03 19:33:16 -0300

    Organização de pastas

---
+++ Começo curso/Aulas/objeto.js
@@ -0,0 +1,10 @@
+const produto = {
+  nome: "laptop",
+  preco: 5000,
+  disponibilidaede: true,
+};
+
+
+console.log(Object.keys(produto)); // chave em uma lista
+console.log(Object.values(produto)); // valores em uma lista
+console.log(Object.entries(produto)); // listas contendo chave e valor
