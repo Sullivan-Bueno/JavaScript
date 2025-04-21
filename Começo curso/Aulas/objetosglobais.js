66256d019385399be81677722e17d55408ec54e9
Sullivan Bueno <sullivanbhs@gmail.com>
2025-04-21 19:56:36 -0300

    math e data

---
+++ Começo curso/Aulas/objetosglobais.js
@@ -0,0 +1,16 @@
+// Math e Date
+
+console.log(Math.PI);
+console.log(Math.E);
+
+const raizQuadrada = Math.sqrt(4);
+console.log(raizQuadrada);
+
+const potencia = Math.pow(2, 3);
+console.log(potencia);
+
+let numero = 3.45;
+console.log(Math.round(numero));
+
+let aleatorio = Math.random()*10;
+console.log(aleatorio.toFixed());
