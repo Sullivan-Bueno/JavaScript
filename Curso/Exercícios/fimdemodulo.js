73093dd888b6d0dd73d381cce0b1fc9958d91e2d
Sullivan <sullivanbhs@gmail.com>
2025-05-22 07:17:51 -0300

    Finalização exercicios do inicio de módulo

--- Curso/Exercícios/fimdemodulo.js
+++ Curso/Exercícios/fimdemodulo.js
@@ -70,3 +70,125 @@ function gastosmaior(gastos) {
 
 // console.log(gastosmaior(gastos));
 
+let numeros = [10, 5, 20, 15, 20, 20, 20];
+
+function media(array) {
+  let soma = array.reduce(function (soma, i) {
+    return soma + i;
+  });
+  return soma / array.length;
+}
+
+// console.log(media(numeros));
+
+function palindromo(palavra) {
+  let palavra2 = "";
+  for (let i = palavra.length - 1; i >= 0; i--) {
+    palavra2 += palavra[i];
+  }
+  if (palavra === palavra2) {
+    return "é um palíndromo";
+  } else {
+    return "não é um palíndromo";
+  }
+}
+
+// console.log(palindromo("cachorro"));
+
+function primo(numero) {
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return "Não é numero primo";
+    } else {
+      return "É um numero primo";
+    }
+  }
+}
+
+// console.log(primo(31));
+
+function quantasPalavras(frase) {
+  let arrayFrase = frase.split(" ");
+  return arrayFrase.length;
+}
+
+// console.log(quantasPalavras("Olá, tudo bem?"));
+
+function distancia(p1, p2) {
+  if (p1 < p2) {
+    return "Pessoa um está mais próxima";
+  } else if (p2 < p1) {
+    return "Pessoa dois está mais próxima";
+  } else {
+    return "Estão na mesma distância";
+  }
+}
+
+// console.log(distancia(30, 30));
+
+let despesas = [150, 80, 200, 60, 120];
+
+function altoGasto(despesas) {
+  let array = [];
+  for (let despesa of despesas) {
+    if (despesa < 100) {
+      array.push("Gasto controlado");
+    } else {
+      array.push("Alto gasto");
+    }
+  }
+  return array;
+}
+
+// console.log(altoGasto(despesas));
+
+const investimentos = [5000, 2000, 15000];
+const nome = "Sullivan";
+
+function investimento(valor, nome) {
+  valor.sort(function (a, b) {
+    return a - b;
+  }); // Ordena os valores do menor pro maior
+  let investimentoFinal = [];
+  for (let investimento in valor) {
+    investimentoFinal.push({
+      investimento: valor[investimento],
+      nome: nome,
+    });
+  }
+  return investimentoFinal;
+}
+
+// console.log(investimento(investimentos, nome));
+
+function codificar(frase) {
+  let fraseNova = frase.toLowerCase();
+  let palavraCodificada = "";
+  const result = {
+    a: "1",
+    á: "1",
+    à: "1",
+    e: "2",
+    é: "2",
+    è: "2",
+    i: "3",
+    í: "3",
+    ì: "3",
+    o: "4",
+    ó: "4",
+    ò: "4",
+    u: "5",
+    ú: "5",
+    ù: "5",
+  }; // Tentativa de evitar uso de IF ELSE
+  for (let letra of fraseNova) {
+    if (letra in result) {
+      palavraCodificada += result[letra];
+    } else {
+      palavraCodificada += letra;
+    }
+  }
+  return palavraCodificada;
+}
+
+// console.log(codificar("A casa e o sol"));
