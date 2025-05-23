0865b304d7e78dab57edec4b635cf702a6f19444
Sullivan Bueno <sullivanbhs@gmail.com>
2025-05-23 20:15:44 -0300

    Atualização simulação

--- Curso/Exercícios/simulacao.js
+++ Curso/Exercícios/simulacao.js
@@ -1,15 +1,27 @@
-function investimento(valor, meses) {
-  let rendimento = valor + valor * (meses * 0.01);
-  // let formatado = rendimento.toFixed(2);
-  let formatado = rendimento.toLocaleString("pt-BR", {
+/* Requisito 1: Simulação de Investimento
+❖ Crie uma função que simule o rendimento de um investimento com base em um valor
+inicial, uma taxa mensal de rendimento e um período de meses.
+❖ A função deve retornar o saldo final após o período especificado, com duas casas
+decimais. */
+
+function investimento(valor, meses, taxa) {
+  let aux = valor;
+  for (let i = 0; i < meses; i++) {
+    aux += aux * taxa;
+  }
+  let formatado = aux.toLocaleString("pt-BR", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
-
   return formatado;
 }
 
-// console.log(investimento(1000, 5));
+let saldoInvestimento = investimento(1000, 5, 0.01);
+
+/* Requisito 2: Gerenciamento de Despesas
+❖ Crie uma função que receba um objeto representando as despesas mensais em
+diferentes categorias.
+❖ A função deve calcular e retornar o total de despesas. */
 
 const despesas = [250, 43, 99.75, 540, 540, 630];
 
@@ -24,7 +36,12 @@ function gerenciarDespesas(despesas) {
   });
 }
 
-// console.log(gerenciarDespesas(despesas));
+let totalDespesas = gerenciarDespesas(despesas);
+
+/* Requisito 3: Obtenção do Mês Atual
+❖ Crie uma função que retorne o nome do mês atual em formato string.
+❖ A função deve utilizar um array de nomes dos meses para identificar o mês atual. */
+
 const dataAtual = new Date();
 
 function obterMesAtual(data) {
@@ -43,7 +60,28 @@ const meses = {
   9: "Setembro",
   10: "Outubro",
   11: "Novembro",
-  12: "Dezembro"
+  12: "Dezembro",
 };
 
-// console.log(meses[obterMesAtual(dataAtual)]);
+let data = meses[obterMesAtual(dataAtual)];
+
+/* Requisito 4: Geração de Relatório Financeiro
+❖ Crie uma função que gere um relatório financeiro com base no investimento inicial, taxa
+de rendimento, duração do investimento, despesas mensais, meta de investimento e
+meta de orçamento.
+❖ O relatório deve incluir:
+■ O mês atual.
+■ O saldo final do investimento.
+■ O total de despesas.
+■ Uma mensagem de economia ou excedente em relação ao orçamento.
+■ Uma verificação se a meta de investimento foi atingida.
+■ Um resumo das despesas por categoria.
+ */
+
+function gerarRelatorio() {
+  console.log(`O mês atual é: ${data}`);
+  console.log(`O saldo final do investimento é: R$ ${saldoInvestimento}`);
+  console.log(`O total de despesas é: ${totalDespesas}`);
+}
+
+gerarRelatorio();
